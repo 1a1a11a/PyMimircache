bd8430a757ee0b5949e88661555aa6c273569b9f
Jason Yang <juncheng.yang@emory.edu>
2016-06-30 02:28:20 +0000

    update conf

--- docs/conf.py
+++ docs/conf.py
@@ -80,8 +80,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    # 'sphinx.ext.viewcode',
+    # 'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
