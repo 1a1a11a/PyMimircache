6ad3d9c4cc6547d9c618ec1b75371d3d3136eb4d
Jason Yang <juncheng.yang@emory.edu>
2016-08-25 15:08:34 -0400

    clean twoDPlots

--- mimircache/profiler/twoDPlots.py
+++ mimircache/profiler/twoDPlots.py
@@ -1,17 +1,5 @@
-import os
-import pickle
-from collections import deque
-from multiprocessing import Array, Process, Queue
-
-import matplotlib
 import matplotlib.ticker as ticker
-import mimircache.c_heatmap as c_heatmap
-import numpy as np
 from matplotlib import pyplot as plt
-
-from mimircache.cacheReader.csvReader import csvReader
-from mimircache.cacheReader.vscsiReader import vscsiReader
-from mimircache.profiler.LRUProfiler import LRUProfiler
 from mimircache.utils.printing import *
 from mimircache.profiler.cHeatmap import cHeatmap
 
@@ -35,7 +23,6 @@ def cold_miss_2d(reader, mode, time_interval, figname="cold_miss2d.png"):
 
     cold_miss_list = [0] * (len(break_points) - 1)
     seen_set = set()
-    never_see = 0
     for i in range(len(break_points) - 1):
         never_see = 0
         for j in range(break_points[i + 1] - break_points[i]):
@@ -72,26 +59,11 @@ def draw2d(l, **kwargs):
         plt.title(kwargs['title'])
 
     plt.savefig(filename, dpi=600)
-    # plt.show()
+    try:
+        plt.show()
+    except:
+        pass
     colorfulPrint("red", "plot is saved at the same directory")
     plt.clf()
 
 
-
-
-
-
-if __name__ == "__main__":
-    pass
-    # TIME_INTERVAL = 10000000000
-    # from mimircache.cacheReader.vscsiReader import vscsiCacheReader
-    # PATH = '/run/shm/traces/'
-    #
-    # reader = vscsiCacheReader('../data/trace.vscsi')
-    # for f in os.listdir(PATH):
-    #     if f.endswith('vscsitrace'):
-    #         reader = vscsiCacheReader(PATH + f)
-    #         request_num_2d(reader, 'r', TIME_INTERVAL,
-    #                        figname='0627_request_num/'+f+'_request_num_'+str(TIME_INTERVAL)+'.png')
-    # request_num_2d(reader, 'r', 10000000)
-    # cold_miss_2d(reader, 'r', 10000000)
